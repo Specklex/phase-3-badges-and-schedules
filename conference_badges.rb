6e5011884b7f7470c985e15f34fe2b8589deb9ed
Jaden Lund <lundjaden@gmail.com>
2022-08-30 13:56:58 -0400

    commit tree f4d97f47bf72a3bf0b6a913596401a6182e735c4

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator people
+    array = []
+   people.each{|name|array << badge_maker(name)}
+   array
+end
+
+def assign_rooms people
+    array = []
+    people.each_with_index do |name, index|
+      array <<  "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    array
+end
+
+def printer people
+    batch_badge_creator(people).each {|names| puts names}
+    assign_rooms(people).each { |room| puts room}
+end
